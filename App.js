42a05314d099d30f2971545d3c6f5d7d124dc4b0
Sean Chapman <65049586+schapman41@users.noreply.github.com>
2023-01-12 14:59:51 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import mygif from './assets/mygif.gif';
+
+function App() {
+  return (
+    <div>
+      <img src={mygif} alt="My gif"/>
+    </div>
+  );
+}
+
+<p>The gif is saved in the src/assets folder and you should save your gifs there too.</p>
+
+export default App;
